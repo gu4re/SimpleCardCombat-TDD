3dd06a375cd004603bcbb94d908a00a410a801b9
ER4UG.reverse <99036688+gu4re@users.noreply.github.com>
2023-03-17 09:23:58 +0100

    Initial Commit

---
+++ src/test/java/es/codeurjc/ais/BookServiceTest.java
@@ -0,0 +1,88 @@
+package es.codeurjc.ais;
+
+import es.codeurjc.ais.book.BookDetail;
+import es.codeurjc.ais.book.BookService;
+import es.codeurjc.ais.book.OpenLibraryService;
+import es.codeurjc.ais.notification.NotificationService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.client.HttpClientErrorException;
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+/**
+ * This class contains all tests about unitary testing
+ * Proving the logic of a component
+ * @author Diego Picazo, Lara Fernandez
+ * @since 2023-03-07
+ */
+@DisplayName("Testing BookService class ...")
+public class BookServiceTest {
+	static BookService bookService = null;
+	static OpenLibraryService openLibraryService = null;
+	static NotificationService notificationService = null;
+	static String success;
+	static String fail;
+	@BeforeAll
+	public static void setUp(){
+		// Instance used for both Tests
+		bookService = new BookService((openLibraryService = mock(OpenLibraryService.class)),
+				(notificationService = mock(NotificationService.class)));
+		success = "NotificationService: SUCCESS!";
+		fail = "NotificationService: FAILED!";
+	}
+	@Test
+	@DisplayName("Got 2 magic books so a notification info is registered")
+	public void testUNIT_Magic(){
+		/* Given */
+		int magic_books;
+		final int NUM_BOOKS = 2;
+		/* When */
+		when(openLibraryService.searchBooks("magic", 10))
+				.thenReturn(Arrays.asList(
+						new OpenLibraryService.BookData(null, "/work/01", null, null, null),
+						new OpenLibraryService.BookData(null, "/work/02", null, null, null)));
+		magic_books = bookService.findAll("magic").size();
+		notificationService.info(success);
+		/* Then */
+		Assertions.assertEquals(NUM_BOOKS, magic_books);
+		// Verify success message
+		verify(notificationService).info(success);
+	}
+	@Test
+	@DisplayName("Got null book so a notification error is registered")
+	public void testUNIT_NonRecover(){
+		/* Given */
+		Optional<BookDetail> bookDetailFail;
+		/* When */
+		// invalid id
+		when(openLibraryService.getBook("anyString()"))
+				.thenThrow(new HttpClientErrorException(HttpStatusCode.valueOf(404)));
+		bookDetailFail = bookService.findById("anyString()");
+		notificationService.error(fail);
+		/* Then */
+		Assertions.assertSame(Optional.empty(), bookDetailFail,
+				"bookDetail should be empty");
+		// Verify error message
+		verify(notificationService).error(fail);
+	}
+	@Test
+	@DisplayName("EXTRA: Checked when valid book ID is passed")
+	// EXTRA test checking what happens if a good ID is passed
+	public void testUNIT_EXTRA(){
+		/* Given */
+		Optional<BookDetail> bookDetailGood;
+		// random valid id of an existing book
+		when(openLibraryService.getBook("OL259010W"))
+				.thenReturn(
+						new OpenLibraryService.BookData(null, "/work/01",
+								null, new Integer[1], new String[]{"HELLO WORLD"}));
+		bookDetailGood = bookService.findById("OL259010W");
+		Assertions.assertNotSame(Optional.empty(), bookDetailGood,
+				"bookDetail should not be empty");
+	}
+}
---
+++ src/test/java/es/codeurjc/ais/ReviewsTest.java
@@ -0,0 +1,108 @@
+package es.codeurjc.ais;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+/**
+ * This class contains all tests about E2E testing
+ * Proving the web interface using Selenium
+ * @author Diego Picazo, Lara Fernandez
+ * @since 2023-03-07
+ */
+@SpringBootTest(classes = Application.class,
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DisplayName("Testing web interface ...")
+public class ReviewsTest {
+	@LocalServerPort
+	int port;
+	private WebDriver driver;
+	
+	@BeforeAll
+	public static void setUp(){
+		WebDriverManager.firefoxdriver().setup();
+	}
+	@BeforeEach
+	public void setUpTest(){
+		driver = new FirefoxDriver();
+		driver.get("http://localhost:"+this.port+"/");
+	}
+	@AfterEach
+	public void teardown(){
+		if (driver != null)
+			driver.quit();
+	}
+	@Test
+	@DisplayName("Checked that the first drama book has a drama tag")
+	public void testSELE_DramaTag(){
+		/* When */
+		driver.findElement(By.cssSelector(".ui.input"))
+				.findElement(By.name("topic")).sendKeys("drama");
+		driver.findElement(By.cssSelector(".ui.input"))
+				.findElement(By.id("search-button")).click();
+		driver.findElement(By.cssSelector(".book:nth-child(1)")).click();
+		/* Then */
+		Assertions.assertNotNull(driver.findElement(By.id("drama")),
+				"Drama tag should be in this book");
+	}
+	@Test
+	@DisplayName("Checked the creation of a 'The Way of Kings' review")
+	public void testSELE_KingsReview(){
+		/* Given */
+		final String NICKNAME = "Rodolfo";
+		final String INFO = "I love this book!";
+		/* When */
+		driver.findElement(By.cssSelector(".ui.input"))
+				.findElement(By.name("topic")).sendKeys("epic fantasy");
+		driver.findElement(By.cssSelector(".ui.input"))
+				.findElement(By.id("search-button")).click();
+		driver.findElement(By.cssSelector(".ui.list"))
+				.findElement(By.id("The Way of Kings")).click();
+		driver.findElement(By.cssSelector(".ui.secondary.segment"))
+				.findElement(By.cssSelector(".ui.form"))
+				.findElement(By.name("nickname")).sendKeys(NICKNAME);
+		driver.findElement(By.cssSelector(".ui.secondary.segment"))
+				.findElement(By.cssSelector(".ui.form"))
+				.findElement(By.name("content")).sendKeys(INFO);
+		driver.findElement(By.cssSelector(".ui.secondary.segment"))
+				.findElement(By.cssSelector(".ui.form"))
+				.findElement(By.id("add-review")).click();
+		/* Then */
+		Assertions.assertTrue(() -> {
+			Assertions.assertEquals(NICKNAME, driver.findElement(By.className("comment"))
+						.findElement(By.className("author")).getText(),
+				"Nickname should be Rodolfo");
+			Assertions.assertEquals(INFO, driver.findElement(By.className("comment"))
+						.findElement(By.className("text")).getText(),
+				"Content should be 'I love this book!'");
+			return true;
+		});
+	}
+	@Test
+	@DisplayName("Checked the bad creation of an empty review")
+	public void testSELE_WordsReview(){
+		/* When */
+		driver.findElement(By.cssSelector(".ui.input"))
+				.findElement(By.name("topic")).sendKeys("epic fantasy");
+		driver.findElement(By.cssSelector(".ui.input"))
+				.findElement(By.id("search-button")).click();
+		driver.findElement(By.cssSelector(".ui.list"))
+				.findElement(By.id("Words of Radiance")).click();
+		driver.findElement(By.cssSelector(".ui.secondary.segment"))
+				.findElement(By.cssSelector(".ui.form"))
+				.findElement(By.name("nickname")).sendKeys("");
+		driver.findElement(By.cssSelector(".ui.secondary.segment"))
+				.findElement(By.cssSelector(".ui.form"))
+				.findElement(By.name("content")).sendKeys("");
+		driver.findElement(By.cssSelector(".ui.secondary.segment"))
+					.findElement(By.cssSelector(".ui.form"))
+					.findElement(By.id("add-review")).click();
+		/* Then */
+		Assertions.assertNotNull(driver.findElement(By.cssSelector(".ui.comments"))
+				.findElement(By.cssSelector(".ui.negative.message")),
+				"Error message should not be null");
+	}
+}
