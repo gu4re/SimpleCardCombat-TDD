69be72acef4c609ae21f91c827f5d73827b16717
gu4re <99036688+gu4re@users.noreply.github.com>
2023-03-18 13:51:59 +0100

    updated

--- src/main/java/es/codeurjc/ais/Card.java
+++ src/main/java/es/codeurjc/ais/Card.java
@@ -1,7 +1,5 @@
 package es.codeurjc.ais;
 
-import es.codeurjc.ais.Position;
-
 public class Card {
     public Card(String name, int attack, int defense, Position position) {
 
--- src/test/java/es/codeurjc/ais/InitialCaseTest.java
+++ src/test/java/es/codeurjc/ais/InitialCaseTest.java
@@ -1,13 +1,19 @@
 package es.codeurjc.ais;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 @DisplayName("Initial Case examples ...")
 public class InitialCaseTest {
+    static String expected;
+    @BeforeEach
+    public void setUp(){
+        expected = "";
+    }
     @Test
     @DisplayName("First Example")
-    public void Test1() {
+    public void test1() {
         String expected = """
                 Carta 1 (3000/2500/Posición: Ataque) vs Carta 2
                 (2500/2100/Posición: Ataque) -> Gana Carta 1. Defensor pierde 500
@@ -15,7 +21,18 @@ public class InitialCaseTest {
         Card card_1 = new Card("Carta 1", 3000, 2500, Position.Attack);
         Card card_2 = new Card("Carta 2", 2500, 2100, Position.Attack);
         String result = Combat.combat(card_1, card_2);
-        Assertions.assertEquals(result, expected);
+        Assertions.assertEquals(expected, result);
     }
-    
+    @Test
+    @DisplayName("Second Example")
+    public void test2(){
+        expected = """
+                Carta 1 (1200/1000/Posición: Ataque) vs Carta 2
+                (1500/1500/Posición: Ataque) -> Gana Carta 2. Atacante pierde 200
+                puntos. Carta 1 destruido/a.""";
+        es.codeurjc.ais.Card card_1 = new es.codeurjc.ais.Card("Carta 1", 3000, 2500, es.codeurjc.ais.Position.Attack);
+        es.codeurjc.ais.Card card_2 = new es.codeurjc.ais.Card("Carta 2", 2500, 2100, es.codeurjc.ais.Position.Attack);
+        String result = es.codeurjc.ais.Combat.combat(card_1, card_2);
+        Assertions.assertEquals(expected, result);
+    }
 }
