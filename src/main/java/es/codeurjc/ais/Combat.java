c788965e56c71a37aba8f1bec2519dfd92c4f60e
gu4re <99036688+gu4re@users.noreply.github.com>
2023-03-22 22:42:09 +0100

    TERMINADO

--- src/main/java/es/codeurjc/ais/Combat.java
+++ src/main/java/es/codeurjc/ais/Combat.java
@@ -3,75 +3,78 @@ package es.codeurjc.ais;
 import org.jetbrains.annotations.NotNull;
 
 public class Combat {
+    // Class Variables
     private static final StringBuilder combat_resolution = new StringBuilder();
     private static final String NOT_SUPPORTED_YET = "Not supported yet";
-    private Combat(){}
-    private static void attackVsAttack(@NotNull Card card1, @NotNull Card card2){
+    private static final String BOTH_CARDS_DESTROYED = " Ambas cartas destruidas.";
+    private static final String CARD2_DESTROYED = " Carta 2 destruido/a.";
+    private static final String EMPTY = "";
+    private static final String CARD1_NAME = "Carta 1";
+    private static int lostPoints;
+    private Combat(){} // Avoiding creation of a Combat Object
+    private static void attackVsAttack(@NotNull Card card1, @NotNull Card card2) throws UnsupportedOperationException{
+        lostPoints = (card2.getEffect() != null && card2.getEffect().equals(Position.EFFECT.PRESSURE))
+                ? Math.abs(card1.getAttack() - card2.getAttack()) / 2
+                : Math.abs(card1.getAttack() - card2.getAttack());
         switch (Integer.signum(Integer.compare(card1.getAttack(), card2.getAttack()))) {
             case -1 -> {
+                combat_resolution.append(String.format("Gana Carta 2. Atacante pierde %d puntos.", lostPoints));
                 if (card1.getEffect() != null)
                     combat_resolution.append((card1.getEffect().equals(Position.EFFECT.IMMORTAL)
-                            ? "Gana Carta 2. Atacante pierde 200 puntos."
-                            : "Gana Carta 2. Atacante pierde 500 puntos. Ambas cartas destruidas."));
+                            ? EMPTY
+                            : BOTH_CARDS_DESTROYED));
                 else
-                    combat_resolution.append("Gana Carta 2. Atacante pierde 300 puntos. Carta 1 destruido/a.");
+                    combat_resolution.append(String.format(" %s destruido/a.", CARD1_NAME));
             }
-            case 0 -> {
-                if (card1.getEffect() != null && card1.getEffect().equals(Position.EFFECT.IMMORTAL))
-                    combat_resolution.append("Empate. Carta 2 destruido/a.");
-                else
-                    combat_resolution.append("Empate. Ambas cartas destruidas.");
-            }
-            case 1 -> {
-                if (card2.getEffect() != null && card2.getEffect().equals(Position.EFFECT.IMMORTAL))
-                    combat_resolution.append("Gana Carta 1. Defensor pierde 100 puntos.");
-                else
-                    combat_resolution.append("Gana Carta 1. Defensor pierde 500 puntos. Carta 2 destruido/a.");
-            }
+            case 0 -> combat_resolution.append(String.format("Empate.%s",
+                        (card1.getEffect() != null && card1.getEffect().equals(Position.EFFECT.IMMORTAL)
+                                ? CARD2_DESTROYED
+                                : BOTH_CARDS_DESTROYED)));
+            case 1 -> combat_resolution.append(String.format("Gana %s. Defensor pierde %d puntos.%s", CARD1_NAME,
+                    lostPoints, (card2.getEffect() != null && card2.getEffect().equals(Position.EFFECT.IMMORTAL)
+                            ? EMPTY
+                            : CARD2_DESTROYED)));
             default -> throw new UnsupportedOperationException(NOT_SUPPORTED_YET);
         }
     }
-    private static void attackVsDefense(@NotNull Card card1, @NotNull Card card2){
+    private static void attackVsDefense(@NotNull Card card1, @NotNull Card card2) throws UnsupportedOperationException{
         // Local variables
         final int DEFENSE_THRESHOLD = 2000;
+        lostPoints = (card1.getEffect() != null && card1.getEffect().equals(Position.EFFECT.PRESSURE))
+                ? Math.abs(card1.getAttack() - card2.getDefense()) / 2
+                : Math.abs(card1.getAttack() - card2.getDefense());
         switch (Integer.signum(Integer.compare(card1.getAttack(), card2.getDefense()))) {
-            case -1 -> {
-                if (card1.getEffect() != null && card1.getEffect().equals(Position.EFFECT.MORTAL_TOUCH)
-                    && card2.getDefense() < DEFENSE_THRESHOLD)
-                    combat_resolution.append("Gana Carta 2. Atacante pierde 500 puntos. Carta 2 destruido/a.");
-                else // Por aqui se cuela el test 16 de Presión
-                    combat_resolution.append("Gana Carta 2. Atacante pierde 1000 puntos.");
-            }
-            case 0 -> combat_resolution.append("Empate.")
-                        .append((card1.getEffect() != null && card2.getEffect() != null &&
-                                 card1.getEffect().equals(Position.EFFECT.MORTAL_TOUCH)
-                                 && card2.getEffect().equals(Position.EFFECT.MORTAL_TOUCH))
-                                 ? " Ambas cartas destruidas." : "");
+            case -1 -> combat_resolution.append(String.format("Gana Carta 2. Atacante pierde %d puntos.%s", lostPoints,
+                        (card1.getEffect() != null && card1.getEffect().equals(Position.EFFECT.MORTAL_TOUCH)
+                                && card2.getDefense() < DEFENSE_THRESHOLD)
+                                ? CARD2_DESTROYED
+                                : EMPTY));
+            case 0 -> combat_resolution.append("Empate.").append((card1.getEffect() != null
+                    && card2.getEffect() != null && card1.getEffect().equals(Position.EFFECT.MORTAL_TOUCH)
+                    && card2.getEffect().equals(Position.EFFECT.MORTAL_TOUCH))
+                    ? BOTH_CARDS_DESTROYED
+                    : EMPTY);
             case 1 -> {
-                combat_resolution.append("Gana Carta 1.");
+                combat_resolution.append(String.format("Gana %s.", CARD1_NAME));
                 if (card2.getEffect() == null)
-                    combat_resolution.append(" Carta 2 destruido/a.");
+                    combat_resolution.append(CARD2_DESTROYED);
                 else if (card2.getEffect().equals(Position.EFFECT.MORTAL_TOUCH)
                         && card1.getDefense() < DEFENSE_THRESHOLD)
-                    combat_resolution.append(" Ambas cartas destruidas.");
+                    combat_resolution.append(BOTH_CARDS_DESTROYED);
             }
             default -> throw new UnsupportedOperationException(NOT_SUPPORTED_YET);
         }
     }
     private static @NotNull String effectToString(@NotNull Position.EFFECT effect){
-        if (effect.toString().equals("NA"))
-            return "N/A";
-        else if(effect.toString().equals("IMMORTAL"))
-            return "Inmortal";
-        else if (effect.toString().equals("MORTAL_TOUCH"))
-            return "Toque mortal";
-        else if (effect.toString().equals("PRESSURE"))
-            return "Presión";
-        return effect.toString();
+        return switch (effect) {
+            case NA -> "N/A";
+            case IMMORTAL -> "Inmortal";
+            case MORTAL_TOUCH -> "Toque mortal";
+            case PRESSURE -> "Presión";
+        };
     }
     private static @NotNull String getCardInfo(@NotNull Card card){
         // Local variables
-        final String CARD1_NAME = "Carta 1";
         StringBuilder cardInfoBuilder = new StringBuilder();
         int cardNumber = card.getName().equals(CARD1_NAME) ? 1 : 2;
         cardInfoBuilder.append((String.format("Carta %d (%d/%d/Posición: %s", cardNumber, card.getAttack(),
@@ -86,12 +89,11 @@ public class Combat {
         // Local variables
         final String ERROR_MESSAGE = "La carta atacante no puede estar en una posición de Defensa";
         final int ZERO = 0;
-        // Clear StringBuilder
+        // Clearing StringBuilder at the beginning of the method
         combat_resolution.setLength(ZERO);
         if (card1.getPosition().equals(Position.DEFENSE))
             throw new IllegalPositionException(ERROR_MESSAGE);
-        combat_resolution.append(getCardInfo(card1))
-                .append(" vs ").append(getCardInfo(card2)).append(" -> ");
+        combat_resolution.append(getCardInfo(card1)).append(" vs ").append(getCardInfo(card2)).append(" -> ");
         switch (card2.getPosition()){
             case ATTACK -> attackVsAttack(card1, card2);
             case DEFENSE -> attackVsDefense(card1, card2);
